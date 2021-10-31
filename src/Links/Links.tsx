7747fccaa61b4425f5389ee1107d293240c0edcb
Ben Centra <bencentra@Bens-MacBook-Pro.local>
2021-10-30 22:18:15 -0400

    check JSON version, handle missing theme fields

--- src/Links/Links.tsx
+++ src/Links/Links.tsx
@@ -5,6 +5,7 @@ import { Theme, useStylesheet } from './useStylesheet'
 export interface JsonResponse {
   links: Link[]
   theme: Theme
+  version: string
 }
 
 export interface Link {
@@ -14,14 +15,29 @@ export interface Link {
 }
 //#endregion
 
-async function fetchJSON(url = './example.json') {
+//#region render links by JSON version
+const renderLinksVersion1_0_0 = (links: Link[]) => links.map(link => (
+  <a key={link.href} href={link.href} className='no-underline'>
+    <div className='link mb-4 p-4 text-center'>
+      <div className='text-2xl mb-2'>{link.title}</div>
+      <div>{link.description}</div>
+    </div>
+  </a>
+))
+
+const LinkRenderers = {
+  '1.0.0': renderLinksVersion1_0_0
+} as any
+//#endregion
+
+async function fetchJSON(url = './liiinks.json') {
   const response = await fetch(url)
   return response.json()
 }
 
 export const Links = () => {
   const [json, setJson] = React.useState({} as JsonResponse)
-  const { theme, links } = json
+  const { version, theme, links } = json
   useStylesheet(theme)
 
   React.useEffect(() => {
@@ -29,19 +45,22 @@ export const Links = () => {
   }, [])
 
   if (!links) {
-    return <>Loading...</>
+    return (
+      <div className='text-center'>Loading...</div>
+    )
+  }
+
+  const render = LinkRenderers[version] as Function
+
+  if (!render) {
+    return (
+      <div className='text-center text-red-800'>Error: Invalid JSON version.</div>
+    )
   }
 
   return (
     <>
-      {links.map(link => (
-        <a key={link.href} href={link.href} className='no-underline'>
-          <div className='link mb-4 p-4 text-center'>
-            <div className='text-2xl mb-2'>{link.title}</div>
-            <div>{link.description}</div>
-          </div>
-        </a>
-      ))}
+      {render(links)}
     </>
   )
 }
